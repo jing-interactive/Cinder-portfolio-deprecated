985c3a3e9df337587fa1392258c1920d8ec4bc26
vinjn <vinjn.z@gmail.com>
2011-09-26 23:31:10 +0800

    + big mouth vfx

--- FaceOff/src/FaceOffApp.cpp
+++ FaceOff/src/FaceOffApp.cpp
@@ -1,4 +1,5 @@
 #include "cinder/app/AppBasic.h"
+#include "cinder/Utilities.h"
 #include "cinder/Thread.h"
 #include "cinder/Capture.h"
 #include "cinder/TriMesh.h"
@@ -37,12 +38,42 @@ struct dirty_misc
 	}
 };
 
+struct face_element_t
+{
+	ci::Vec2f* center;
+	vector<ci::Vec2f*> items;
+	int n_items;
+	void init(vector<ci::Vec2f>& points, int idx_center, int* idx_array, int array_size)
+	{
+		center =&points[idx_center];
+		items.resize(array_size);
+		for (int i=0;i<array_size;i++)
+			items[i] = &points[idx_array[i]];
+		n_items = array_size;
+	}
+	void scale(const ci::Vec2f& k)
+	{
+		for (int i=0;i<n_items;i++)
+		{
+			ci::Vec2f dir = *items[i] - *center;
+			*items[i] = *center + dir*k;
+		}
+	}
+	void rotate(const ci::Vec2f& k)
+	{
+	}
+	void translate(const ci::Vec2f& k)
+	{
+	}
+}left_eye, right_eye, mouth;
+
 class FaceOffApp : public AppBasic {
 public:
 	//cinder routines
 	void setup();
 	void shutdown();
 	void mouseDown( MouseEvent event );	
+	void keyDown( KeyEvent event );
 	void update();
 	void draw();
 
@@ -50,12 +81,14 @@ public:
 	void grabCamera();
 	std::thread grabCameraThread;
 	Capture		mCap;
+	ci::Vec2i	camSize;
 	Surface		surInput;
 	CSimpleMutex mtx_surInput;
 
 	//process
 	void detectFace();
 	std::thread detectFaceThread;
+	void modifyVertices(std::vector<ci::Vec2f>& vertices);
 	cv::CascadeClassifier	mFaceCascade;
 	vector<Rectf>			mFaces;
 	CSimpleEvent			evt_face;
@@ -69,6 +102,7 @@ public:
 	asmfitting fit_asm;
 	std::vector<TriangleData>	face_indices;
 	std::vector<ci::Vec2f>		face_vertices;
+	std::vector<ci::Vec2f>		face_vertices2;
 	CSimpleMutex				mtx_triList;
 
 	//rendering
@@ -76,14 +110,17 @@ public:
 	bool running;
 	ci::TriMesh triMesh;
 	gl::VboMesh vboMesh;
+	bool	faceFrameVisible;
 };
 
 void FaceOffApp::setup()
 {
 	running = true;
+	faceFrameVisible = false;
 	try {
 		mCap = Capture( 640, 480 );
 		mCap.start();
+		camSize = mCap.getSize();
 	}
 	catch( ... ) {
 		console() << "Failed to initialize capture" << std::endl;
@@ -107,17 +144,21 @@ void FaceOffApp::update()
 		tex_frame = surInput;
 	LOCK_END();
 
-	LOCK_START(mtx_triList);
-	triMesh.clear();
-	for (int i=0;i<face_vertices.size();i++)
+	LOCK_START(mtx_triList); 
+	if (face_vertices.size() > 0)
 	{
-		triMesh.appendVertex(ci::Vec3f(face_vertices[i],0));
-		triMesh.appendColorRGB( Color( 0.1, 0.7, 0.5 ) );
-	}
-	for (int i=0;i<face_indices.size();i++)
-	{
-		const TriangleData& tri = face_indices[i];
-		triMesh.appendTriangle(tri.idx[0], tri.idx[1], tri.idx[2]);
+		triMesh.clear();
+		for (int i=0;i<face_vertices.size();i++)
+		{
+			triMesh.appendVertex(ci::Vec3f(face_vertices[i],0));
+		//	triMesh.appendColorRGBA( ColorA( 1, 1, 1,1.0 ) );
+			triMesh.appendTexCoord(ci::Vec2f(face_vertices2[i].x/(float)camSize.x,face_vertices2[i].y/(float)camSize.y));
+		}
+		for (int i=0;i<face_indices.size();i++)
+		{
+			const TriangleData& tri = face_indices[i];
+			triMesh.appendTriangle(tri.idx[0], tri.idx[1], tri.idx[2]);
+		}
 	}
 	LOCK_END();
 }
@@ -133,11 +174,10 @@ void FaceOffApp::draw()
 	// clear out the window with black
 	gl::clear( Color( 0, 0, 0 ) );
 
-	gl::color( Color( 1, 1, 1) );
+	gl::color( ColorA( 1, 1, 1,0.5) );
 	gl::draw(tex_frame);
-	tex_frame.disable();
 
-	// draw the faces as transparent yellow rectangles
+	// draw the faces as transparent yellow circles
 	gl::color( ColorA( 1, 1, 0, 0.25f ) );
 	LOCK_START(mtx_mFaces);
 	for (int i=1;i<mFaces.size();i++) //<--- i begins with 1
@@ -147,11 +187,20 @@ void FaceOffApp::draw()
 	}
 	LOCK_END();
 
-	if (triMesh.hasColorsRGB())//test if..
+	if (triMesh.getNumVertices() > 0)//test if..
 	{
-		gl::enableWireframe();
+		gl::color( ColorA( 1, 1, 1,0.5) );
+		tex_frame.enableAndBind();
 		gl::draw(triMesh);
-		gl::disableWireframe();
+		tex_frame.unbind();
+
+		if (faceFrameVisible)
+		{
+			gl::color( Color( 0, 1, 0) );
+			gl::enableWireframe();
+			gl::draw(triMesh);
+			gl::disableWireframe();
+		}
 	}
 }
 
@@ -268,29 +317,67 @@ void FaceOffApp::runASM()
 		bool flag = fit_asm.ASMSeqSearch(shape_list[0], &ipl_img, fr_asm_frame, true, n_iterations);
 		if(flag) fit_asm.Draw(&ipl_img, shape_list[0]);
 
-#ifdef _DEBUG
-		show_mat(frame);
-		cv::waitKey(1);
-#endif
 		int n_pts = shape_list[0].NPoints();
 		facePoints.resize(n_pts);
+		char buff[10];
 		for (int i=0;i<n_pts;i++)
 		{
 			const Point2D32f& pt = shape_list[0][i];
 			facePoints[i].x = pt.x;
 			facePoints[i].y = pt.y;
+			vDrawText(&ipl_img, pt.x, pt.y, itoa(i, buff, 10));
 		}
 
+#ifdef _DEBUG
+		show_mat(frame);
+		cv::waitKey(1);
+#endif
 		//why i use temp_tri ? for sake of multi-thread latency time
 		std::vector<TriangleData> temp_tri = ciTri::triangulate(facePoints, facePoints.size(), false);
 
 		LOCK_START(mtx_triList);
 		face_indices = temp_tri;
 		face_vertices = facePoints;
+		modifyVertices(face_vertices);
+		face_vertices2 = facePoints;
 		LOCK_END();
 
 		runASMThread.yield();
 	}
+}
+
+void FaceOffApp::keyDown( KeyEvent event )
+{
+	int code = event.getCode();
+	if (code == event.KEY_ESCAPE)
+		quit();
+	if (code == event.KEY_SPACE)
+		faceFrameVisible = !faceFrameVisible;
+
+}
+
+void FaceOffApp::modifyVertices(std::vector<ci::Vec2f>& vertices)
+{
+#define BIG_EYE_MOUTH
+#ifdef BIG_EYE_MOUTH
+	//eyes
+	static int left_eye_idx[] = {27,28,29,30};
+	static int right_eye_idx[] = {32,33,34,35};
+	left_eye.init(vertices, 31, left_eye_idx, 4);
+	right_eye.init(vertices, 36, right_eye_idx, 4);
+
+	float k = 2.5;
+	left_eye.scale(ci::Vec2f(k*0.9,k));
+	right_eye.scale(ci::Vec2f(k*0.9,k));
+
+	//mouth
+	vector<int> mouth_idx;
+	for (int idx=48;idx<=65;idx++)
+		mouth_idx.push_back(idx);
+	mouth.init(vertices, 66, &mouth_idx[0], mouth_idx.size());
+	k = 1.5;
+	mouth.scale(ci::Vec2f(k,k));
+#endif
 }
 
 CINDER_APP_BASIC( FaceOffApp, RendererGl )
