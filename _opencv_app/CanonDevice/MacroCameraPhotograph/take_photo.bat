08bb0e1b8cfd7cae4f4411f69d24db6d1f4e287b
vinjn <vinjn.z@gmail.com>
2012-03-25 20:19:04 +0800

    #Cutout# a temp version, needs to confirm the usage

---
+++ _opencv_app/CanonDevice/MacroCameraPhotograph/take_photo.bat
@@ -0,0 +1,2 @@
+cd bin
+MacroCameraPhotograph -n "test.jpg"
--- _opencv_app/Cutout/src/main.cpp
+++ _opencv_app/Cutout/src/main.cpp
@@ -10,27 +10,34 @@ int main(int argc, char **argv)
 	if (argc != 3)
 	{
 		fprintf(stdout, "usage: %s inputImageName threshNum\n", argv[0]);
+		return -1;
 	}
-	string name = argv[1];
+	string name(argv[1]);
 	int thresh = atoi(argv[2]);
 
-	Mat3b src = imread(name);
+	Mat3b src = cvLoadImage(argv[1]);
+	show_mat(src);
 
-	Mat1b mask;
-	vGrayScale(src, mask);
-	vThreshInv(mask, thresh);
+	Mat3b frame = src.clone();
+	floodFill(frame, Point(1,1),Scalar(0,0,0), NULL, Scalar::all(thresh), Scalar::all(thresh));
+	show_mat(frame);
+
+	Mat1b gray;
+	vGrayScale(frame, gray);
 
 	// Create mat with alpha channel
 	cv::Mat4b dst(src.size());
 
-	for (int j = 0; j < dst.rows; ++j) {
-		for (int i = 0; i < dst.cols; ++i) {
+	for (int j = 0; j < dst.rows; ++j) 
+	{
+		for (int i = 0; i < dst.cols; ++i)
+		{
 			cv::Vec4b& rgba = dst(j, i);
-			cv::Vec3b& rgb = src(j, i);
+			cv::Vec3b& rgb = src(j, i); 
 			rgba[0] = rgb[0];
 			rgba[1] = rgb[1];
 			rgba[2] = rgb[2];
-			if (mask(j,i) > 0)
+			if (gray(j,i) > 0)
 				rgba[3] = 255;
 			else
 				rgba[3] = 0;
@@ -41,7 +48,7 @@ int main(int argc, char **argv)
 		std::vector<int> params;
 		params.push_back(CV_IMWRITE_PNG_COMPRESSION);
 		params.push_back(9);
-		cv::imwrite(name+".png", dst, params);
+		cvSaveImage(string(name+".png").c_str(), &(IplImage)dst, &params[0]);
 	}
 	catch (std::runtime_error& ex) {
 		fprintf(stderr, "Exception converting image to PNG format: %s\n", ex.what());
@@ -49,4 +56,6 @@ int main(int argc, char **argv)
 	}
 
 	fprintf(stdout, "Saved PNG file with alpha data.\n");
+
+	waitKey();
 }
