0024ef492d5a5073278f0edb1ef6b431d722e017
vinjn <vinjn.z@gmail.com>
2012-12-07 23:19:19 +0800

    + irrKlangBasic

--- _cinder_app/irrKlangBasic/src/irrKlangBasicApp.cpp
+++ _cinder_app/irrKlangBasic/src/irrKlangBasicApp.cpp
@@ -1,14 +1,20 @@
 #include "cinder/app/AppBasic.h"
 #include "cinder/audio/Output.h"
 #include "cinder/audio/Io.h"
+#include "cinder/audio/SourceFileWindowsMedia.h"
 
 #include "Resources.h"
 
 using namespace ci;
 using namespace ci::app;
 
-class AudioPlaybackApp : public AppBasic {
- public:
+#define CATCH_EXCEPTION( ExceptionType ) \
+	catch ( ExceptionType& e ){\
+	console() << #ExceptionType << e.what() << std::endl;}
+
+
+class irrKlangBasicApp : public AppBasic {
+public:
 	void setup();
 	void mouseDown( MouseEvent );
 	void draw();
@@ -22,29 +28,27 @@ private:
 			if ( src )
 				mAudioSources.push_back( src );
 		}
-		catch ( Exception& e )
-		{
-			console() << e.what() << std::endl;
-		}
+		CATCH_EXCEPTION( AssetLoadExc )
+		CATCH_EXCEPTION( audio::IoException )
 	}
 	std::vector<audio::SourceRef> mAudioSources;
 };
 
-void AudioPlaybackApp::setup()
+void irrKlangBasicApp::setup()
 {
-	loadAudio( "getout.ogg" );
+	//loadAudio( "getout.ogg" );
 	loadAudio( "ophelia.mp3" );
 }
 
-void AudioPlaybackApp::mouseDown( MouseEvent event )
+void irrKlangBasicApp::mouseDown( MouseEvent event )
 {
 	audio::Output::play( mAudioSources.front() );
 }
 
-void AudioPlaybackApp::draw()
+void irrKlangBasicApp::draw()
 {
 	glClearColor( 0.1f, 0.1f, 0.1f, 1.0f );
 	glClear( GL_COLOR_BUFFER_BIT );
 }
 
-CINDER_APP_BASIC( AudioPlaybackApp, RendererGl )
+CINDER_APP_BASIC( irrKlangBasicApp, RendererGl )
