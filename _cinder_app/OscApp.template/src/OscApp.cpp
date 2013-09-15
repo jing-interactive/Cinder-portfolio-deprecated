d79579f5a294df15987a81d7624ac1db1755db70
vinjn <vinjn.z@gmail.com>
2013-09-15 23:03:46 +0800

    improve OscApp.template

--- _cinder_app/OscApp.template/src/OscApp.cpp
+++ _cinder_app/OscApp.template/src/OscApp.cpp
@@ -2,6 +2,7 @@
 #include "cinder/gl/gl.h"
 #include "cinder/osc/OscSender.h"
 #include "cinder/osc/OscListener.h"
+#include "../../../_common/MiniConfig.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -9,19 +10,27 @@ using namespace std;
 
 struct OscApp : public AppBasic 
 {
-    static const int kPort = 3000;
-    
     void prepareSettings(Settings *settings)
     {
         settings->setWindowPos(0, 0);
         settings->setWindowSize(640, 480);
+
+        readConfig();
     }
 
     void setup()
     {
-        mListener.setup(kPort);
+        mListener.setup(OSC_PORT);
     //  sender.setup();
         mListener.registerMessageReceived(this, &OscApp::onOscMessage);
+    }
+
+    void keyUp(KeyEvent event)
+    {
+        if (event.getCode() == KeyEvent::KEY_ESCAPE)
+        {
+            quit();
+        }
     }
 
     void onOscMessage(const osc::Message* msg)
