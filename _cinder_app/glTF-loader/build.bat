9a4b7cc12f745e897463e44a380315d161598ee1
vinjn <vinjn.z@gmail.com>
2013-10-31 13:49:39 +0800

    #_cinder_app replace build.bat

--- _cinder_app/glTF-loader/build.bat
+++ _cinder_app/glTF-loader/build.bat
@@ -1,1 +1,15 @@
-msbuild vc9\CiApp.sln /p:Configuration=Release /m
+@set vcbuild_path="%VS90COMNTOOLS%\..\..\VC\vcpackages\vcbuild.exe"
+
+@if exist %vcbuild_path% goto do_build 
+
+@echo ERROR: vcbuild.exe not found in
+@echo %vcbuild_path%
+@echo Please edit this batch file to set correct path to vcbuild.exe
+@pause
+
+@goto end
+
+:do_build
+@%vcbuild_path% /MP vc9\CiApp.vcproj Release
+
+:end
