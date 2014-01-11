4be26bd7a02d010c16f26dd73cf2a83bb3c62faf
vinjn <vinjn.z@gmail.com>
2014-01-11 16:38:06 +0800

    finish VisionApp.template

--- _cinder_app/VisionApp.template/src/CiApp.cpp
+++ _cinder_app/VisionApp.template/src/CiApp.cpp
@@ -1,10 +1,9 @@
 #include "cinder/app/AppBasic.h"
 #include "cinder/ImageIo.h"
-#include "cinder/Camera.h"
+#include "cinder/Capture.h"
 
 #include "cinder/gl/gl.h"
 #include "cinder/gl/Texture.h"
-#include "cinder/gl/Vbo.h"
 #include "cinder/params/Params.h"
 
 #include "../../../_common/MiniConfig.h"
@@ -13,12 +12,20 @@ using namespace ci;
 using namespace ci::app;
 using namespace std;
 
+static void updateTextureFromSurface(gl::Texture& dst, const Surface& src)
+{
+    if (dst && src.getSize() == dst.getSize())
+        dst.update(src);
+    else
+        dst = gl::Texture(src);
+}
+
 struct CiApp : public AppBasic 
 {
     void prepareSettings(Settings *settings)
     {
         readConfig();
-        
+
         settings->setWindowPos(0, 0);
         settings->setWindowSize(WIN_WIDTH, WIN_HEIGHT);
     }
@@ -27,6 +34,25 @@ struct CiApp : public AppBasic
     {
         mParams = params::InterfaceGl("params", Vec2i(300, getConfigUIHeight()));
         setupConfigUI(&mParams);
+
+        vector<Capture::DeviceRef> devices(Capture::getDevices());
+        if (devices.empty())
+        {
+            console() << "No valid capture device." << endl;
+            quit();
+        }
+
+        size_t deviceId = 0;
+        try
+        {
+            mCapture = Capture(CAM_WIDTH, CAM_HEIGHT, devices[deviceId]);
+            mCapture.start();
+        }
+        catch (CaptureExc &)
+        {
+            console() << "Unable to initialize device: " << devices[deviceId]->getName() << endl;
+            quit();
+        }
     }
 
     void keyUp(KeyEvent event)
@@ -39,7 +65,11 @@ struct CiApp : public AppBasic
 
     void update()
     {
-    
+        if (mCapture.checkNewFrame())
+        {
+            mCaptureSurf = mCapture.getSurface();
+            updateTextureFromSurface(mCaptureTex, mCaptureSurf);
+        }
     }
 
     void draw()
@@ -47,11 +77,19 @@ struct CiApp : public AppBasic
         gl::setMatricesWindow(getWindowSize());
         gl::clear(ColorA::black());
 
+        if (mCaptureSurf)
+        {
+            gl::draw(mCaptureTex, getWindowBounds());
+        }
+
         mParams.draw();
     }
 
 private:
     params::InterfaceGl mParams;
+    Capture             mCapture;
+    gl::Texture         mCaptureTex;
+    Surface8u           mCaptureSurf;
 };
 
 CINDER_APP_BASIC(CiApp, RendererGl)
